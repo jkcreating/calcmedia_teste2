e466d9ddda80c4be755ce133977a4ab5e228938c
jkcreating <jamilekarinee@gmail.com>
2026-07-28 21:50:39 -0300

    Add files via upload

---
+++ JS/TÓPICOS/nota_1_media.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('form'); 
+form.addEventListener('submit', function(event){
+    event.preventDefault(); 
+    
+    const nota1 = document.getElementById('nota1').value; 
+    const nota2 = document.getElementById('nota2').value; 
+
+
+    const nota_resposta = ((Number(nota1))+(Number(nota2))).toFixed(2); 
+    const value = document.getElementById('value'); 
+    let description = ''; 
+    value.classList.add('attention')
+
+    document.getElementById('infos').classList.remove('hidden');
+
+    if (nota_resposta < 4){
+        description = 'abaixo do peso'; 
+    }
+    else{
+        description = 'esta ok esta ok esta ok esta ok esta ok'; 
+        value.classList.remove('attention');
+        value.classList.add('normal')
+    }
+
+
+
+
+    value.textContent = nota_resposta; 
+    document.getElementById('description').textContent = description
+})
+
+
