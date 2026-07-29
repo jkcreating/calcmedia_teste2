74f0826c3c44546b81bcaa60c48d8db06f217f1b
jkcreating <jamilekarinee@gmail.com>
2026-07-28 21:17:15 -0300

    Add files via upload

--- JS/TÓPICOS/script.js
+++ JS/TÓPICOS/script.js
@@ -2,18 +2,26 @@ const form = document.getElementById('form');
 form.addEventListener('submit', function(event){
     event.preventDefault(); 
 
-    const weight = document.getElementById('weight').value; 
-    const height = document.getElementById('height').value; 
+    /*const weight = document.getElementById('weight').value; 
+    const height = document.getElementById('height').value; */
+    
+    const nota1 = document.getElementById('nota1').value; 
+    const nota2 = document.getElementById('nota2').value; 
 
+    /*Média Parcial: [(4xN1)+(6xN2)]/10*/
+    /*const bmi = (weight / (height * height)).toFixed(2); 
+    const value = document.getElementById('value'); 
+    let description = ''; 
+    value.classList.add('attention')*/
 
-    const bmi = (weight / (height * height)).toFixed(2); 
+    const nota_resposta = ((Number(nota1))+(Number(nota2))).toFixed(2); 
     const value = document.getElementById('value'); 
     let description = ''; 
     value.classList.add('attention')
 
     document.getElementById('infos').classList.remove('hidden');
 
-    if (bmi < 4){
+    if (nota_resposta < 4){
         description = 'abaixo do peso'; 
     }
     else{
@@ -25,7 +33,7 @@ form.addEventListener('submit', function(event){
 
 
 
-    value.textContent = bmi; 
+    value.textContent = nota_resposta; 
     document.getElementById('description').textContent = description
 })
 
